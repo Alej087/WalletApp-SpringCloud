92a96dac812ce03ccd019156d281d1c96cd3e4ec
Alej087 <alejo87.gonzalez@gmail.com>
2023-04-02 12:52:01 -0500

    se ajusta el gateway para que traiga la info por catalog de series y movies

---
+++ api-catalog/src/main/java/com/dh/catalog/client/SerieServiceClient.java
@@ -0,0 +1,57 @@
+package com.dh.catalog.client;
+
+import com.dh.catalog.config.LoadBalanceConfiguration;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FeignClient(name="serie-service")
+@LoadBalancerClient(value="serie-service",configuration = LoadBalanceConfiguration.class)
+public interface SerieServiceClient {
+
+    @GetMapping("/api/v1/series/{genre}")
+    List<SerieDTO> getSeriesByGenre(@PathVariable (value = "genre") String genre);
+
+    @Setter
+    @Getter
+    class SerieDTO{
+
+        private String id;
+        private String name;
+        private String genre;
+        private List<Season> seasons = new ArrayList<>();
+
+        @AllArgsConstructor
+        @NoArgsConstructor
+        @Setter
+        @Getter
+        public static class Season {
+
+            private Integer seasonNumber;
+            private List<Chapter> chapters = new ArrayList<>();
+
+            @AllArgsConstructor
+            @NoArgsConstructor
+            @Setter
+            @Getter
+            public static class Chapter {
+
+                private String name;
+                private Integer number;
+                private String urlStream;
+
+
+            }
+
+        }
+
+    }
+}
--- api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
+++ api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
@@ -2,6 +2,10 @@ package com.dh.catalog.controller;
 
 import com.dh.catalog.client.MovieServiceClient;
 
+import com.dh.catalog.client.SerieServiceClient;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,14 +19,32 @@ import java.util.List;
 public class CatalogController {
 
 	private final MovieServiceClient movieServiceClient;
+	private final SerieServiceClient serieServiceClient;
 
-	public CatalogController(MovieServiceClient movieServiceClient) {
+	public CatalogController(MovieServiceClient movieServiceClient, SerieServiceClient serieServiceClient) {
 		this.movieServiceClient = movieServiceClient;
+		this.serieServiceClient = serieServiceClient;
 	}
 
-	@GetMapping("/{genre}")
+	/*@GetMapping("/{genre}")
 	ResponseEntity<List<MovieServiceClient.MovieDto>> getGenre(@PathVariable String genre) {
 		return ResponseEntity.ok(movieServiceClient.getMovieByGenre(genre));
+	}*/
+
+	@GetMapping("/{genre}")
+	MovieSerieDTO getSerieByGenre(@PathVariable String genre) {
+		List<MovieServiceClient.MovieDto> movie =  movieServiceClient.getMovieByGenre(genre);
+		List<SerieServiceClient.SerieDTO> serie = serieServiceClient.getSeriesByGenre(genre);
+		MovieSerieDTO movieSerieDTO = new MovieSerieDTO(movie,serie);
+		return movieSerieDTO;
+	}
+
+	@Setter
+	@Getter
+	@Builder
+	static class MovieSerieDTO{
+		private List<MovieServiceClient.MovieDto> movies;
+		private List<SerieServiceClient.SerieDTO> series;
 	}
 
 }
